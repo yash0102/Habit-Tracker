42678af19c7674201af22e0124d17f82f9a18dad
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-06-02 21:51:38 +0530

    Setup Express Server

---
+++ controller/homeController.js
@@ -0,0 +1,3 @@
+module.exports.home = (req, res) => {
+    return res.send('<h1> Hello Controller </h1>');
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 8000;
+
+app.use('/', require('./routes'));
+
+app.listen(port , (err) => {
+    if (err) {
+        console.error('Error in running the server',err);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+})
